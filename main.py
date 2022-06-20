32306f7d71c85ec72893f7aa363ba83a40319738
shibyx <100292269+shibyx@users.noreply.github.com>
2022-06-20 20:06:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from datetime import datetime
 from datetime import date
 app = Flask(__name__)
 
-current_date = datetime.now().date()
-current_time = datetime.now().time()
 
 @app.route('/')
 def index():
+    current_date = datetime.now().date()
+    current_time = datetime.now().time()
     return f"Hello world. Today is {current_date}. Time is {current_time}"
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
