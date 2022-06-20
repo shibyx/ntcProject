9c89c32ba722336e8084873a768086a15f610fbf
yoxu <young.xukka@gmail.com>
2022-06-20 23:53:00 +0300

    main.py tz update

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import os
 from flask import Flask
 from datetime import datetime
 from datetime import date
+import pytz
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
 def index():
+    mskTz = pytz.timezone("Europe/Moscow")
+    timeInMSK = datetime.now(mskTz)
     current_date = datetime.now().date()
-    current_time = datetime.now().time()
+    current_time = timeInMSK.strftime("%H:%M:%S")
     return f"Hello world. Today is {current_date}. Time is {current_time}"
 
 
