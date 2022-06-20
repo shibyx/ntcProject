8dc8d92fa1b5fa9c69abd0d63796c11c8e926403
shibyx <100292269+shibyx@users.noreply.github.com>
2022-06-20 20:12:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from datetime import date
 app = Flask(__name__)
 
+port = int(os.environ.get('PORT', 5000))
+
 
 @app.route('/')
 def index():
@@ -13,5 +15,4 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
